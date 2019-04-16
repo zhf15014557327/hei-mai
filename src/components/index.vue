857e6a7ac03274aa6efb4e6b6bbf91a2811637e4
Your Name <118735992@.con>
2019-04-16 21:14:15 +0800

    完成项目初始化全局样式的导入

---
+++ src/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="content-index">
+
+    </div>
+</template>
+<script>
+export default {
+    name:"index",
+}
+</script>
+<style>
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入全局样式
+import "./assets/css/style.css"
 
 Vue.config.productionTip = false
 
